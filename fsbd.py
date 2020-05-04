81dca942ce053eb1a603f1a736a9e60e25a8d597
Tom Lynch <tom@machinelearningdeveloper.com>
2020-05-03 20:24:17 -0500

    Download the list of free books. Close #3.

--- fsbd.py
+++ fsbd.py
@@ -1,4 +1,8 @@
 from configparser import ConfigParser
+from io import BytesIO
+
+import pandas as pd
+import requests
 
 config = ConfigParser()
 
@@ -6,3 +10,12 @@ config.read('config.ini')
 list_url = config['urls']['list_url']
 example_book_url = config['urls']['example_book_url']
 book_url_column = config['columns']['book_url_column']
+book_list_path = config['paths']['book_list_path']
+
+res = requests.get(list_url)
+
+f = BytesIO(res.content)
+
+df = pd.read_excel(f)
+
+df.to_excel(book_list_path)

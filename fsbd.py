6b417d01f781af4e31fbd5b077e8a18b1e29f089
Tom Lynch <tom@machinelearningdeveloper.com>
2020-05-03 21:31:14 -0500

    Determine book subject. Close #7.

--- fsbd.py
+++ fsbd.py
@@ -12,6 +12,7 @@ example_book_url = config['urls']['example_book_url']
 book_url_column = config['columns']['book_url_column']
 book_list_path = config['paths']['book_list_path']
 downloads_base_dir = config['paths']['downloads_base_dir']
+book_subject_column = config['columns']['book_subject_column']
 
 res = requests.get(list_url)
 
@@ -20,3 +21,10 @@ f = BytesIO(res.content)
 df = pd.read_excel(f)
 
 df.to_excel(book_list_path)
+
+def get_subject(book, subject_column):
+    return book[subject_column].values[0]
+
+book = df[df[book_url_column] == example_book_url]
+book_subject = get_subject(book, book_subject_column)
+print(book_subject)
